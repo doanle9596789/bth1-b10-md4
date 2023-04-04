0e589f4a1026bd75f1b9126ad5a0b18bab947b53
doanle95956789 <doanle95956@gmail.com>
2023-04-04 09:33:08 +0700

    bth1-b10

---
+++ src/main/java/com/example/bth1/controller/CounterController.java
@@ -0,0 +1,22 @@
+package com.example.bth1.controller;
+
+
+import com.example.bth1.model.Counter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@SessionAttributes("counter")
+public class CounterController {
+
+    @ModelAttribute("counter")
+    public Counter setUpCounter() {
+        return new Counter();
+    }
+
+    @GetMapping("/")
+    public String get(@ModelAttribute("counter") Counter counter) {
+        counter.increment();
+        return "/index";
+    }
+}
